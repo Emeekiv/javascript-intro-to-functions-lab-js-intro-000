1de3d53c5e624e73ef32cff29df4cc4fc40850df
Edwin Meek <emeekiv@gmail.com>
2019-12-13 16:19:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function logWhisper(string){
   console.log(string.toLowerCase());
 }
 function sayHiToGrandma(string){
-  if(string===string.toLowerCase){
+  if(string===string.mixedCase){
     return "I can\'t hear you!"
   }
   if(string===string.toUpperCase){
